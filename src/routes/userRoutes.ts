2a2dd0950a277113038156d913a3c9bb4efff57a
Kshitij <kshitij2809@gmail.com>
2025-12-18 12:01:32 +0530

    Your commit message here

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,10 +4,14 @@ import { upload } from "../middlewares/upload";
 
 const userRouter = Router();
 
-userRouter.post("/signup",upload.fields([
+userRouter.post(
+  "/signup",
+  upload.fields([
     { name: "aadhaarFile", maxCount: 1 },
     { name: "panCardFile", maxCount: 1 },
-  ]), signup);
+  ]),
+  signup,
+);
 userRouter.post("/login", login);
 userRouter.post("/logout", logout);
 userRouter.post("/contact", contact);
