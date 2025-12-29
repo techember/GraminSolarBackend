58010a25b3f07640a2d6f28f14030dddadd46ca0
Kshitij <kshitij2809@gmail.com>
2025-12-29 15:52:01 +0530

    cron bugs fixes part 3 time updated

--- src/utils/cron.ts
+++ src/utils/cron.ts
@@ -8,7 +8,7 @@ export const initCronJobs = () => {
 
   cronInitialized = true;
 
-  cron.schedule("0 * * * *", async () => {
+  cron.schedule("0 0 * * *", async () => {
     try {
       console.log("Running order processing cron");
       await processOrderEmails();
